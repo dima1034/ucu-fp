2a64fb168c3466832a0d657e02248d19ee206e6a
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2024-04-21 14:33:16 +0100

    tmp

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,5 @@
 import threading
+
 from dotenv import load_dotenv
 from reactivex import Observable, operators as ops
 from reactivex.scheduler import ThreadPoolScheduler
@@ -10,11 +11,15 @@ from src.message import GithubEvent
 load_dotenv()
 
 thread_pool_scheduler = ThreadPoolScheduler(max_workers=10)
+# new_thread_scheduler = NewThreadScheduler()
 
 if __name__ == "__main__":
+    words = ["python", "java"]
+
     repos_storage = dict()
     new_repos = repo.filter_new_repos(repos_storage)
-    source: Observable[GithubEvent] = fetch_data_as_observable("python")
+
+    source: Observable[GithubEvent] = fetch_data_as_observable(words[0])
     new_repos_obs = new_repos(source).pipe(ops.observe_on(thread_pool_scheduler))
 
     new_repos_obs.subscribe(
